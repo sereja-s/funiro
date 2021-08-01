265a8ed28a1522729532a2234c7553209e9cd015
sereja-s <serejasuvorov1979@gmail.com>
2021-08-01 21:13:03 +0300

    Шапка, подвал, слайдер ( html )

--- Funiro/js/script.js
+++ Funiro/js/script.js
@@ -22,10 +22,26 @@ window.onload = function () {
 
 	function documentActions(e) {
 		const targetElement = e.target;
-		if (window.innerWidth > 768) {
+		// появляется выпадающее меню при щелчке на стрелочке (при ширине экрана меньше 769)
+		if (window.innerWidth > 319 /* && isMobile.any() */) {
 			if (targetElement.classList.contains('menu__arrow')) {
 				targetElement.closest('.menu__item').classList.toggle('_hover');
 			}
+			if (!targetElement.closest('.menu__item') && document.querySelector('.menu__item._hover')) {
+				document.querySelector('.menu__item._hover').classList.remove('_hover');
+			}
+			// пункты выпадающего меню можно закрывать щелчком в любои месте
+			/* if (!targetElement.closest('.menu__item') && document.querySelectorAll('.menu__item._hover').length > 0) {
+				removeClasses(document.querySelectorAll('.menu__item._hover'), "_hover");
+			} */
+		}
+		// добавлят строчку при щелчке на иконке(поиск) на малых экранах
+		if (targetElement.classList.contains('search-form__icon')) {
+			document.querySelector('.search-form').classList.toggle('_active');
+		}
+		// убираем строчку при клике на любом свободном месте
+		else if (!targetElement.closest('.search-form') && document.querySelector('.search-form._active')) {
+			document.querySelector('.search-form').classList.remove('_active');
 		}
 	}
 }
